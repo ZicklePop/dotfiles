11fec4e03c9d482649990ccbc3c18ec517a996ce
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2020-08-16 16:52:54 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
-if [ ! -d "$HOME/.zsh/zsh-autosuggestions" ]; then
-  git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
 
 if [ ! -d "$HOME/.vim_runtime" ]; then

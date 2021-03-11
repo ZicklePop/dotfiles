196f4d54ea8170a08794dc032c83ce758784ff17
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2021-03-11 09:49:17 -0800

    swapping to SpaceVim

--- install.sh
+++ install.sh
@@ -6,9 +6,8 @@ if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
 
-if [ ! -d "$HOME/.vim_runtime" ]; then
-  git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-  sh ~/.vim_runtime/install_awesome_vimrc.sh
+if [ ! -d "$HOME/.SpaceVim" ]; then
+  curl -sLf https://spacevim.org/install.sh | bash
 fi
 
 if [[ `uname` == 'Darwin' ]]; then
@@ -28,3 +27,5 @@ cp -rf .editorconfig ~/.editorconfig > /dev/null 2>&1
 cp -rf .gitconfig ~/.gitconfig > /dev/null 2>&1
 cp -rf .macos ~/.macos > /dev/null 2>&1
 cp -rf .zshrc ~/.zshrc > /dev/null 2>&1
+cp -rf .SpaceVim.d ~ 2>&1
+
